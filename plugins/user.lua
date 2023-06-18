08414a02fe4b55560034a5869050e5ee586dca6e
kca2250 <2250katok@gmail.com>
2023-06-18 22:50:38 +0900

    neogitを追加

--- plugins/user.lua
+++ plugins/user.lua
@@ -11,4 +11,19 @@ return {
   -- },
   "github/copilot.vim",
   lazy = false,
+  {
+    "TimUntersberger/neogit",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    dependencies = {
+      "sindrets/diffview.nvim",
+    },
+    config = function()
+      require("neogit").setup {
+        integrations = {
+          diffview = true,
+        },
+      }
+    end,
+  },
 }
