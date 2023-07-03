880370e58fa33872323811c4085d62c7b89c1203
kca2250 <2250katok@gmail.com>
2023-07-03 09:30:21 +0900

    refactaring

--- plugins/user.lua
+++ plugins/user.lua
@@ -11,29 +11,4 @@ return {
   -- },
   "github/copilot.vim",
   lazy = false,
-  {
-    "timuntersberger/neogit",
-    version = "*", -- use for stability; omit to use `main` branch for the latest features
-    event = "verylazy",
-    dependencies = {
-      "sindrets/diffview.nvim",
-    },
-    config = function()
-      require("neogit").setup {
-        integrations = {
-          diffview = true,
-        },
-      }
-    end,
-  },
-  {
-    "kylechui/nvim-surround",
-    tag = "*", -- Use for stability; omit to use `main` branch for the latest features
-    config = function()
-      require("nvim-surround").setup({
-        Configuration here, or leave empty to use defaults
-      })
-      end,
-    },
-  },
 }
