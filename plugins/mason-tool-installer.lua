58cfa49d7cacffcbd56025d4639a33d0178ec214
kca2250 <2250katok@gmail.com>
2023-07-11 06:51:25 +0900

    add plugins

---
+++ plugins/mason-tool-installer.lua
@@ -0,0 +1,17 @@
+return {
+  ensure_installed = {
+    --LSP
+    "lua-language-server",
+    "typescript-language-server",
+    "vim-language-server",
+    "html-lsp",
+    "css-lsp",
+    "json-lsp",
+
+    -- Formatter
+    "prettierd",
+
+    -- Linter,
+    "eslint_d",
+  }
+}
